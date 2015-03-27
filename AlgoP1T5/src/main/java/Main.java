b5e9f3bbd387a1e879a79d0c88e7f7e2df06a3bd
Artem Mikhalevitch <artyommikhalevitch@gmail.com>
2015-03-27 11:29:27 +0300

    Task 5 initial commit

---
+++ AlgoP1T5/src/main/java/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Artem_Mikhalevitch on 3/18/15.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        StdOut.println("Hello World");
+    }
+}
---
+++ AlgoP1T5/src/test/java/MainTest.java
@@ -0,0 +1,15 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+
+/**
+ * Created by ASUS on 26.03.2015.
+ */
+public class MainTest {
+
+    @Test
+    public void test() {
+        Assert.assertTrue(true);
+        System.out.println("Test H/W");
+    }
+}
