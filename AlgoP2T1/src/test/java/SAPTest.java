b823b24d4f7ac364afb155cc5df4d66ef0b76348
Amstel90 <artyommikhalevitch@gmail.com>
2015-05-07 23:42:20 +0300

    SAP class fully implemented and tested.

--- AlgoP2T1/src/test/java/SAPTest.java
+++ AlgoP2T1/src/test/java/SAPTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -5,9 +6,62 @@ import org.junit.Test;
  */
 public class SAPTest {
 
-    @Test
-    public void test() {
+    public static final String folder = "D:\\Projects\\Algos\\AlgoP2T1\\src\\resources\\";
 
+    Digraph twoVertex = new Digraph(2);
+    SAP twoVertexSap;
+
+    {
+        twoVertex.addEdge(0, 1);
+        twoVertexSap = new SAP(twoVertex);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void length1() {
+        twoVertexSap.length(0, 2);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void length2() {
+        twoVertexSap.length(2, 0);
+    }
+
+    @Test
+    public void length3() {
+
+        SAP sap = new SAP(getDigraphByFileName("digraph1.txt"));
+
+        Assert.assertEquals(3, sap.length(9, 12));
+    }
+
+    @Test
+    public void length4() {
+
+        SAP sap = new SAP(getDigraphByFileName("digraph2.txt"));
+
+        Assert.assertEquals(2, sap.length(1, 5));
+    }
+
+    @Test
+    public void ancestor1() {
+
+        SAP sap = new SAP(getDigraphByFileName("digraph1.txt"));
+
+        Assert.assertEquals(1, sap.ancestor(3, 11));
+    }
+
+    @Test
+    public void ancestor2() {
+
+        SAP sap = new SAP(getDigraphByFileName("digraph2.txt"));
+
+        Assert.assertEquals(0, sap.ancestor(1, 5));
+    }
+
+    private Digraph getDigraphByFileName(String fileName) {
+
+        In in = new In(folder + fileName);
+        return new Digraph(in);
     }
 
 }
